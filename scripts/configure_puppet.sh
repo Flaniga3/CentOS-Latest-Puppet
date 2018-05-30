1f895780ab1c1b80489fa38620a2f59370d1057b
Joseph Flanigan <joeflanigan@northwesternmutual.com>
2018-05-30 13:04:01 -0500

    Fixing configure script

--- scripts/configure_puppet.sh
+++ scripts/configure_puppet.sh
@@ -1,4 +1,5 @@
-sudo puppet config set masterport 32771
-sudo puppet config set server puppet
-sudo puppet config set --section agent environment development
-sudo puppet config set --section agent certname "centos-test-server-$(date +"%s")"
+sudo -E su
+puppet config set masterport 32771
+puppet config set server puppet
+puppet config set --section agent environment development
+puppet config set --section agent certname "centos-test-server-$(date +"%s")"
